b82807ba39161adb97f325ef6eb3a7decc0df6e5
Jonas Eriksson <jonaseriksson84@gmail.com>
2019-11-04 09:27:54 +0100

    latest

--- src/App.svelte
+++ src/App.svelte
@@ -1,70 +1,15 @@
 <script>
-  import Buzzword from "./Buzzword.svelte";
-  const src = "static/jonas-bw-size.png";
-
-  const buzzwords = [
-    {
-      heading: "Frameworks",
-      words: ["React", "Svelte", "Vue", "AngularJS", "Django", "Node", "jQuery"]
-    },
-    {
-      heading: "Languages",
-      words: ["JavaScript", "Python", "HTML5", "CSS3", "Java", "SQL", "Jinja2"]
-    },
-    {
-      heading: "Databases",
-      words: ["postgres, MySQL, DB2"]
-    },
-    {
-      heading: "Tooling",
-      words: [
-        "Webpack",
-        "Rollup",
-        "Gulp",
-        "Grunt",
-        "Yarn",
-        "Karma",
-        "Jasmine",
-        "pytest"
-      ]
-    },
-    {
-      heading: "Version handling & CM",
-      words: ["Git", "CVS", "SVN", "JIRA", "TFS"]
-    },
-    {
-      heading: "Other",
-      words: [
-        "Amazon Web Services (AWS)",
-        "Ansible",
-        "Vagrant",
-        "Jenkins",
-        "Linux",
-        "Scrum",
-        "Web Design",
-        "Project Management"
-      ]
-    }
-  ];
+  import Sidebar from "./Sidebar.svelte";
 </script>
 
 <style type="text/scss">
-  $color: #2f4f4f;
-
-  /* RGB */
-  $color1: rgba(230, 238, 214, 1);
-  $color2: rgba(136, 149, 141, 1);
-  $color3: rgba(96, 109, 93, 1);
-  $color4: rgba(167, 38, 8, 1);
-  $color5: rgba(9, 12, 2, 1);
-
   .cv {
     max-width: 1200px;
     margin: 100px auto;
     // background: url("static/ivory-paper.jpg");
     background-color: #e6e6e8;
     background-size: contain;
-    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
     padding-top: 50px;
     display: flex;
     position: relative;
@@ -80,70 +25,26 @@
       z-index: 1;
     }
 
-    aside {
-      margin-left: 50px;
-      padding: 0 40px;
-      background-color: #dedee0;
-      //box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-      border-right: 2px solid #ddd;
-      max-width: 25%;
-      padding-top: 50px;
-      border-radius: 2px;
-      z-index: 2;
-
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: center;
-
-      .divider {
-        display: block;
-        margin: 0 auto;
-        background-color: #4d4d4f;
-        height: 2px;
-        width: 75%;
-        margin-top: 25px;
-      }
-
-      h2 {
-        text-align: right;
-        font-size: 1.25rem;
-        letter-spacing: 0.1rem;
-        margin-top: 50px;
-      }
-
-      .about p {
-        text-align: justify;
-      }
-
-      .contact-block {
-        text-align: right;
-        width: 100%;
-
-        .fa {
-          width: 15px;
-          margin-left: 15px;
-          color: red;
-          font-size: 1.25rem;
-        }
-      }
-    }
-
     main {
       width: 75%;
       border-radius: 2px;
       z-index: 2;
-    }
+      padding: 50px;
 
-    h1 {
-      color: $color2;
-      display: block;
-      font-size: 3rem;
-      margin: 25px;
-    }
+      color: #e6e6e8;
 
-    img {
-      border-radius: 50%;
-      width: 200px;
+      h1 {
+        font-size: 5rem;
+        margin: 0;
+        text-align: center;
+      }
+
+      p {
+        margin: 0;
+        &:nth-of-type(2) {
+          text-align: right;
+        }
+      }
     }
   }
 </style>
@@ -152,63 +53,13 @@
 
   <div class="background-element" />
 
-  <aside>
-
-    <img {src} alt="Jonas" />
-
-    <div class="contact-block">
-      <h2>CONTACT</h2>
-      <p>
-        070 420 01 75
-        <i class="fa fa-phone" />
-      </p>
-      <p>
-        Havsörnsvägen 85, 123 53 Farsta
-        <i class="fa fa-map-marker" />
-      </p>
-      <p>
-        jonaseriksson84@gmail.com
-        <i class="fa fa-envelope" />
-      </p>
-      <p>
-        www.mynameisjonas.dev
-        <i class="fa fa-globe" />
-      </p>
-    </div>
-
-    <div class="divider" />
-
-    <div class="about">
-      <h2>PROFILE</h2>
-      <p>
-        Jonas is a fullstack web developer with 7 years of professional
-        experience. He particularly enjoys doing frontend development and
-        JavaScript, though also comfortable with backend and API development.
-      </p>
-      <p>
-        Jonas is a social person who thrives in a strong team, but he is also
-        full of initiative and drive – and will quickly learn new processes,
-        environments and technologies on his own. Regardless of project, Jonas
-        always has a professional attitude and a clear focus on delivering
-        results.
-      </p>
-    </div>
-
-    <div class="divider" />
-
-    <div class="buzzwords">
-      <h2>SKILLS</h2>
-      {#each buzzwords as buzzword}
-        <Buzzword heading={buzzword.heading} words={buzzword.words} />
-      {/each}
-    </div>
-  </aside>
+  <Sidebar />
   <main>
-    <h2>Hi, my name is</h2>
+    <p>Hi, my name is</p>
     <h1>Jonas Eriksson</h1>
     <p>
       ... and I am a
-      <strong>Fullstack Web Developer</strong>
+      <strong>FULLSTACK WEB DEVELOPER</strong>
     </p>
   </main>
 </div>
---
+++ src/Sidebar.svelte
@@ -0,0 +1,156 @@
+<script>
+  import Buzzword from "./Buzzword.svelte";
+  const src = "static/jonas-bw-size.png";
+
+  const buzzwords = [
+    {
+      heading: "Frameworks",
+      words: ["React", "Svelte", "Vue", "AngularJS", "Django", "Node", "jQuery"]
+    },
+    {
+      heading: "Languages",
+      words: ["JavaScript", "Python", "HTML5", "CSS3", "Java", "SQL", "Jinja2"]
+    },
+    {
+      heading: "Databases",
+      words: ["postgres, MySQL, DB2"]
+    },
+    {
+      heading: "Tooling",
+      words: [
+        "Webpack",
+        "Rollup",
+        "Gulp",
+        "Grunt",
+        "Yarn",
+        "Karma",
+        "Jasmine",
+        "pytest"
+      ]
+    },
+    {
+      heading: "Version handling & CM",
+      words: ["Git", "CVS", "SVN", "JIRA", "TFS"]
+    },
+    {
+      heading: "Other",
+      words: [
+        "Amazon Web Services (AWS)",
+        "Ansible",
+        "Vagrant",
+        "Jenkins",
+        "Linux",
+        "Scrum",
+        "Web Design",
+        "Project Management"
+      ]
+    }
+  ];
+</script>
+
+<style type="text/scss">
+  aside {
+    margin-left: 50px;
+    padding: 0 40px;
+    background-color: #dedee0;
+    //box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    border-right: 2px solid #ddd;
+    max-width: 25%;
+    padding-top: 50px;
+    border-radius: 2px;
+    z-index: 2;
+
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    img {
+      border-radius: 50%;
+      width: 200px;
+    }
+
+    .divider {
+      display: block;
+      margin: 0 auto;
+      background-color: #4d4d4f;
+      height: 2px;
+      width: 75%;
+      margin-top: 25px;
+    }
+
+    h2 {
+      text-align: right;
+      font-size: 1.25rem;
+      letter-spacing: 0.1rem;
+      margin-top: 50px;
+    }
+
+    .about p {
+      text-align: justify;
+    }
+
+    .contact-block {
+      text-align: right;
+      width: 100%;
+
+      .fa {
+        width: 15px;
+        margin-left: 15px;
+        color: red;
+        font-size: 1.25rem;
+      }
+    }
+  }
+</style>
+
+<aside>
+
+  <img {src} alt="Jonas" />
+
+  <div class="contact-block">
+    <h2>CONTACT</h2>
+    <p>
+      070 420 01 75
+      <i class="fa fa-phone" />
+    </p>
+    <p>
+      Havsörnsvägen 85, 123 53 Farsta
+      <i class="fa fa-map-marker" />
+    </p>
+    <p>
+      jonaseriksson84@gmail.com
+      <i class="fa fa-envelope" />
+    </p>
+    <p>
+      www.mynameisjonas.dev
+      <i class="fa fa-globe" />
+    </p>
+  </div>
+
+  <div class="divider" />
+
+  <div class="about">
+    <h2>PROFILE</h2>
+    <p>
+      Jonas is a fullstack web developer with 7 years of professional
+      experience. He particularly enjoys doing frontend development and
+      JavaScript, though also comfortable with backend and API development.
+    </p>
+    <p>
+      Jonas is a social person who thrives in a strong team, but he is also full
+      of initiative and drive – and will quickly learn new processes,
+      environments and technologies on his own. Regardless of project, Jonas
+      always has a professional attitude and a clear focus on delivering
+      results.
+    </p>
+  </div>
+
+  <div class="divider" />
+
+  <div class="buzzwords">
+    <h2>SKILLS</h2>
+    {#each buzzwords as buzzword}
+      <Buzzword heading={buzzword.heading} words={buzzword.words} />
+    {/each}
+  </div>
+</aside>
